7c346800311ce6272d1e5c04819f4049d2fbc12f
dmytriy <bochkovoi.dmytriy@gmail.com>
2021-10-31 14:28:21 +0200

    js

--- gr_1_bochkovoy_dmytriy_dz_js/js/homework_3.js
+++ gr_1_bochkovoy_dmytriy_dz_js/js/homework_3.js
@@ -1,16 +1,21 @@
+const DIVIDER = 6;
 
-let numberDays = prompt('Введите желаемое количество дней', 0)
+let one = prompt('Введите первое число', 0);
+let two = prompt('Введите второе число', 0);
 
-let lastNumber = Number(numberDays.slice(-1));
+let countMultiplicity = 0;
+let summ = 0;
 
-let lastTwoNumber = Number(numberDays.slice(-2));
+one = +one;
+two = +two;
 
-if (10 < lastTwoNumber && lastTwoNumber <= 14 ) {
-    alert(`${numberDays} дней`);
-} else if (lastNumber === 1) {
-    alert(`${numberDays} день`);
-} else if (1 < lastNumber && lastNumber <= 4) {
-    alert(`${numberDays} дня`);
-} else if (4 < lastNumber && lastNumber <= 9 || lastNumber === 0) {
-    alert(`${numberDays} дней`);
-} 
+for (let i = one; i <= two; i++) {
+    let remains =  i % DIVIDER;
+
+    if(remains === 0) {
+        countMultiplicity++;
+        summ += i
+    }
+}
+
+alert(`Количесво чисел кратных 6 равно ${countMultiplicity}. Сумма этих чисел равна ${summ}`)
