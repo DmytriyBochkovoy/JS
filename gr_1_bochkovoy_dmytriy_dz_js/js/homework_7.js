cb6adee507b7133bd266bc50e9ed4206f646570b
DmytriyBochkovoy <85780204+DmytriyBochkovoy@users.noreply.github.com>
2021-11-10 12:06:57 +0200

    js

---
+++ gr_1_bochkovoy_dmytriy_dz_js/js/homework_7.js
@@ -0,0 +1,50 @@
+function Calculation(options) {
+    this.a = options.a;
+    this.b = options.b;
+    this.matheOperator = options.matheOperator;
+
+    let date = new Date()
+    let nameMatheOperator = ['Сложение', 'Вычитание', 'Умножение', 'Деление']
+
+    if (this.matheOperator === '+') {
+        this.addition = this.a + this.b;
+        getAddResult (this.addition, nameMatheOperator[0], this.a, this.b)
+    } else if (this.matheOperator === '-') {
+        this.subtraction = this.a - this.b;
+        getAddResult (this.subtraction, nameMatheOperator[1], this.a, this.b)
+    } else if (this.matheOperator === '*') {
+        this.multiplication = this.a * this.b;
+        getAddResult (this.multiplication, nameMatheOperator[2], this.a, this.b)
+    } else if (this.matheOperator === '/') {
+        this.division = this.a / this.b;
+        getAddResult (this.division, nameMatheOperator[3], this.a, this.b)
+    } else {
+        console.error('Введены неверные данные')
+    }
+
+    function getAddResult (result, array, a, b) {
+        let dateString = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`
+        let timeString = `${date.getHours()}:${date.getMinutes()}`
+        computationHistory.push(`Выполнены вычисления(${dateString} ${timeString}) ${array} = ${result} (${a}, ${b})`);
+    } //   Формирует строку с параметрами математической операции и добавляет ее в массив выполненных операций.      
+}
+
+let computationHistory = []; //  Память выполненных операций с ограничение по количеству отображаемых.
+let maxCountOperations = 3; //  Ограничивает количество ввода. 
+let maxCountSavedOperations = 2; //  Ограничивает количество сохраненных операций 
+let i = 1;
+
+while (i <= maxCountOperations) {
+    i++;
+    calculator =  new Calculation({
+        a: +prompt('Введите первое значение', 0),
+        matheOperator: prompt('Выберите математическую операцию  из предложенных (+, -, *, /)'),
+        b: +prompt('Введите второе значение', 0),
+    });
+    if (computationHistory.length = maxCountSavedOperations) { 
+        computationHistory.shift(); // Проверка на длину массива, если длинна превысила допустимое значение то удаляется первая выполненная операция. 
+    }
+}
+
+
+console.log(computationHistory);
