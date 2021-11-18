edd45f3440531dac39742b90a7b5077f2406f6a2
DmytriyBochkovoy <85780204+DmytriyBochkovoy@users.noreply.github.com>
2021-11-19 00:40:31 +0200

    js

--- gr_1_bochkovoy_dmytriy_dz_js/js/homework_10.js
+++ gr_1_bochkovoy_dmytriy_dz_js/js/homework_10.js
@@ -1,48 +1,60 @@
-
 fetch('https://jsonplaceholder.typicode.com/users')
 .then((response) => {
-    console.log(response);
     return response.json();
 })
 .then(async (json) => {
+
     let usersArray = json;
     let divArray = [];
-    let buttonArray = []
-
+    
     function createElementsUsers() {
-        document.body.setAttribute('style', 'display: flex; flex-wrap: wrap;')
+        const USER_INFO_ARRAY= ['Name', 'Username', 'Email', 'Website', 'Address', 'City',]
         
-        let usersInfo = document.createElement('button');
-        let usersClear = document.createElement('button');
+        document.body.setAttribute('style', 'display: flex; flex-wrap: wrap;')
 
-        usersInfo.innerHTML = 'information';
-        usersClear.innerHTML = 'to clear';
+        for (let i = 0; i <= usersArray.length - 1; i++) {
+            const USERS_INFO = document.createElement('button');
+            const USERS_CLEAR = document.createElement('button');
 
-        usersInfo.addEventListener('click', () => {
-            alert('geo: ' + 'lat: ' + user.address.geo.lat + ', lng: ' + user.address.geo.lng)
-        })
+            let elemImg = new Image;
+            let pArray = [];
 
-        usersClear.addEventListener('click', () => {
-            divArray[1].remove();
-        })
+            elemImg.src = 'img/user.png';
 
-        for (let i = 0; i <= usersArray.length - 1; i++ ) {
-            user = usersArray[i];
-            
-            divArray[i] = document.createElement('div');
+            USERS_INFO.innerHTML = 'information';
+            USERS_CLEAR.innerHTML = 'to clear';
+
+            USERS_INFO.classList.add('button_info');
+            USERS_CLEAR.classList.add('button_clear');
+
+            USERS_INFO.setAttribute('style', 'margin: 5px; padding: 8px; border-radius: 5px;');
+            USERS_CLEAR.setAttribute('style', 'margin: 5px; padding: 8px; border-radius: 5px;');
+
+            USERS_INFO.onclick = () => {
+                alert('geo: ' + 'lat: ' + usersArray[i].address.geo.lat + ', lng: ' + usersArray[i].address.geo.lng);
+            };
+            USERS_CLEAR.onclick = () => {
+                divArray[i].remove();
+            };
+
+            for (let i = 0; i <= 5; i++) {
+                pArray.push(document.createElement('p'));
+            }
+
+            divArray.push(document.createElement('div'));
             document.body.append(divArray[i]);
-            
-            divArray[i].setAttribute('style', 'background-color: rgb(212, 247, 248); margin: 10px; width: 200px; height: 300px; border: 1px solid green;');
-           
-            divArray[i].innerHTML = `Name - ${usersArray[i].name}. 
-                Username - ${usersArray[i].username}.
-                Email - ${usersArray[i].email}.
-                Website - ${usersArray[i].website}.
-                Address - ${usersArray[i].address.street}.
-                City - ${usersArray[i].address.city}.`;
+            divArray[i].setAttribute('style', 'background-color: #96c6da; wigth: 320px; border-radius: 10px; margin: 10px; padding: 15px; text-align: center;');
 
-            divArray[i].append(usersInfo);
-            divArray[i].append(usersClear);
+            pArray[0].innerHTML = `${USER_INFO_ARRAY[0]} - ${usersArray[i].name}.`;
+           
+            pArray[1].innerHTML = `${USER_INFO_ARRAY[1]} - ${usersArray[i].username}.`;
+            
+            pArray[2].innerHTML = `${USER_INFO_ARRAY[2]} - ${usersArray[i].email}.`;
+            pArray[3].innerHTML = `${USER_INFO_ARRAY[3]} - ${usersArray[i].website}.`;
+            pArray[4].innerHTML = `${USER_INFO_ARRAY[4]} - ${usersArray[i].address.street}.`;
+            pArray[5].innerHTML = `${USER_INFO_ARRAY[5]} - ${usersArray[i].address.city}.`;
+
+            divArray[i].append(elemImg, pArray[0], pArray[1], pArray[2], pArray[3], pArray[4], pArray[5], USERS_INFO, USERS_CLEAR);
         }
     }
     createElementsUsers()
