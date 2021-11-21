beaefccfb0d91d4d3f5230d2d5ad02d9b570a464
DmytriyBochkovoy <85780204+DmytriyBochkovoy@users.noreply.github.com>
2021-11-21 23:39:18 +0200

    js

--- gr_1_bochkovoy_dmytriy_dz_js/js/homework_10.js
+++ gr_1_bochkovoy_dmytriy_dz_js/js/homework_10.js
@@ -2,7 +2,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
 .then((response) => {
     return response.json();
 })
-.then(async (json) => {
+.then((json) => {
 
     let usersArray = json;
     let divArray = [];
